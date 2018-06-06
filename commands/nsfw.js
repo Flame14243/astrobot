177318d55ba290bb2c9491123edd43c83969773c
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-06-07 00:17:34 +0300

    Update nsfw.js

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -3,12 +3,12 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
 let embed = new Discord.RichEmbed()
-.setTitle(":name_badge:  NSFW")
+.setTitle(":underage:  NSFW")
 .setDescription("Bu komutlar kullanmak için nfsw kanalınızın olması gerekmektedir. Komutları odada kimse yokken kullanın :)")
 .addField("`" + message.prefix +"hentai`", "Rastgele hentai resmi atar")
 .addField("`" + message.prefix +"ecchi`", "Rastgele ecchi resmi atar")
 .addField("`" + message.prefix +"neko`", "Rastgele neko resmi atar")
-.addField("`" + message.prefix +"meme`", "Rastgele meme resmi atar.")
+.addField("`" + message.prefix +"memeler`", "Rastgele meme resmi atar.")
 .addField("`" + message.prefix +"popo`", "Rastgele popo resmi atar")
 
 
