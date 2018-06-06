2fc8204c3b5254856a8fbf402fe38cdaf2545a4a
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-06-06 23:27:55 +0300

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -4,27 +4,27 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 message.delete();
   
-  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("<:tickNo:432418492667396097> **| You don't have `MANAGE_MEMBERS` permissions.**");
+  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send(":no_entry_sign: **| Senin `MANAGE_MEMBERS` yetkin yok.**");
   let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(!tomute) return message.reply("Couldn't find user.");
-  if(tomute.hasPermission("MANAGE_MESSAGES")) return message.channel.send("<:tickNo:432418492667396097> **| Can't mute them!**");
-  let muterole = message.guild.roles.find(`name`, "Muted");
+  if(tomute.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":no_entry_sign: **| Susturamıyorum `BAN_MEMBERS` yetkin yok.**");
+  let muterole = message.guild.roles.find(`name`, "Susturuldu");
   
-  if(!muterole) return message.channel.send("<:tickNo:432418492667396097> **| Please create a role named `Muted`.**")
+  if(!muterole) return message.channel.send(":no_entry_sign: **| Sunucuda `Susturuldu` isimli rol yok.**")
   
 
   await(tomute.removeRole(muterole.id));
   let reason = args.join(" ").slice(22)
     let Embed = new Discord.RichEmbed()
-  .setTitle("Un-Mute")
+  .setTitle("Susturma Kaldır")
   .setColor("#fc6400")
-  .addField("User", tomute.user.tag)
-  .addField("Moderator", message.author.tag)
-  .addField("Reason", reason ? reason : "None")
+  .addField("Üye", tomute.user.tag)
+  .addField("Yetkili", message.author.tag)
+  .addField("Sebep", reason ? reason : "None")
   .setTimestamp()
-  message.channel.send("<:tickYes:432418492889694210> **| That member has been un-muted.**")
-let channel= message.guild.channels.find(`name`, 'mod-log')
-if(!channel) return message.channel.send("<:tickNo:432418492667396097> **| Please create a mod-log channel!**")
+  message.channel.send(":white_check_mark:  **| Üyenin susturma yasağı kalktı artık konuşabilir.**")
+let channel= message.guild.channels.find(`name`, 'eoa_unmute')
+if(!channel) return message.channel.send(":name_badge: **| Sunucuda `eoa_unmute` kanalı bulunamadı.**")
   channel.send(Embed)
 
 }
