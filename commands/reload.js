98b8854431cca30256b1d910a5f67bdee740a54c
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-06-06 23:01:11 +0300

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -4,19 +4,19 @@ const fs = require("fs");
 module.exports.run = async (bot, message, args) => {
   let embed = new Discord.RichEmbed()
   .setTitle("Reload")
-  .setDescription("Sorry, the `reload` command can only be executed by the Developer.")
+  .setDescription("Üzgünüm `reload` komutunu sadece yapımcısı @Enes Onur Ata#9427 kullanabilir!")
   .setColor("#cdf785");
-  if(message.author.id !== '414111663076147201') return message.channel.send(embed);
+  if(message.author.id !== '274551537139712001') return message.channel.send(embed);
 
   try{
     delete require.cache[require.resolve(`./${args[0]}.js`)];
   let Aembed = new Discord.RichEmbed()
-  .setTitle("Reloading..")
-  .setDescription(`${args[0]}.js successfully reloaded!`)
+  .setTitle("Tüm Komutlar Yenileniyor..")
+  .setDescription(`${args[0]}.js komutu başarıyla yenilendi!`)
 
     return message.channel.send(Aembed);
      }catch(e){
-     return message.channel.send("There is no such command!")
+     return message.channel.send("Böyle bir komut yok!")
      }
 }; 
 
