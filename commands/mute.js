dbcbd46521f2d8aec090ece830a17928ac4415ef
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-06-06 23:22:57 +0300

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -4,16 +4,16 @@ const ms = require("ms");
 module.exports.run = async (bot, message, args) => {
 message.delete();
   
-  if(!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("<:tickNo:432418492667396097> | You don't have `MANAGE_ROLES` permissions.");
+  if(!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send(":no_entry_sign: **| Senin `MANAGE_ROLES` yetkin yok.**");
   let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(tomute === message.guild.member(message.author)) return message.channel.send("Are you retarted? Why do you wanna mute yourself?")
-  if(!tomute) return message.channel.send("Couldn't find user.");
-  let muterole = message.guild.roles.find(`name`, "Muted");
+  if(tomute === message.guild.member(message.author)) return message.channel.send("Özürlü müsün? Neden kendini Susturmak istiyorsun?")
+  if(!tomute) return message.channel.send(":name_badge: **| Üye bulunamadı.**");
+  let muterole = message.guild.roles.find(`name`, "Susturuldu");
   //start of create role
   if(!muterole){
     try{
       muterole = await message.guild.createRole({
-        name: "Muted",
+        name: "Susturuldu",
         color: "#000000",
         permissions:[]
       })
@@ -32,15 +32,15 @@ message.delete();
 let reason = args.join(" ").slice(22)
   await(tomute.addRole(muterole.id));
   let Embed = new Discord.RichEmbed()
-  .setTitle("Mute")
+  .setTitle("Susturuldu")
   .setColor("#fc6400")
-  .addField("User", tomute.user.tag)
-  .addField("Moderator",message.author.tag)
-  .addField("Reason", reason)
+  .addField("Üye", tomute.user.tag)
+  .addField("Yetkili",message.author.tag)
+  .addField("Sebep", reason)
   .setTimestamp();
- message.channel.send("<:tickYes:432418492889694210> **| That user has been muted.**")
-let channel= message.guild.channels.find(`name`, 'mod-log')
-if(!channel) return message.channel.send("<:tickNo:432418492667396097> **| Please create a mod-log channel!")
+ message.channel.send(":white_check_mark:  **| Üye başarıyla susturuldu.**")
+let channel= message.guild.channels.find(`name`, 'zp_mute')
+if(!channel) return message.channel.send(":name_badge: **| Sunucuda `zp_mute` kanalı bulunamadı.**")
   channel.send(Embed)
   }
 
