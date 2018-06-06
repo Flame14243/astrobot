4a7d188e1771b0f8bf4c1ded2bd6443dd7076f3e
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-06-06 23:18:51 +0300

    Update games.js

--- commands/games.js
+++ commands/games.js
@@ -3,12 +3,12 @@ const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
 
     let embed = new Discord.RichEmbed()
-    .setTitle("<:TAfun:444855091585875979> Games")
-    .addField("`" + message.prefix + "fortnite`", "Check a user's fortnite stats!")
-    .addField("`" + message.prefix +"csgo`", "Check a user's cs:go stats!");
+    .setTitle("<:TAfun:444855091585875979> OYUNLAR")
+    .addField("`" + message.prefix + "fortnite`", "Kullanıcının Fortnite istatistiklerine bak!")
+    .addField("`" + message.prefix +"csgo`", "Kullanıcının CSGO istatistiklerine bak!");
     message.channel.send(embed);
     
 }
 module.exports.help = {
-    name: "games"
+    name: "oyunlar"
 }
