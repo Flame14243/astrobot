62fcb34681d85f024855892e2b724447435dbca7
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-06-06 23:27:32 +0300

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -2,11 +2,11 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let bUser = args[0]
-    if(!bUser) return message.channel.send("Can't find user!");
+    if(!bUser) return message.channel.send(":name_badge: **| Üye bulunamadı.** Lütfen ID'sini girin");
     let bReason = args.join(" ").slice(22);
-    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("<:tickNo:432418492667396097> **| You don't have `BAN_MEMBERS` permissons.**");
+    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(":no_entry_sign: **| Senin `BAN_MEMBERS` yetkin yok.**");
 
-  message.channel.send("<:tickYes:432418492889694210> **| That member has been un-banned.**").then(msg => msg.delete({timeout: 20000}));
+  message.channel.send("<:tickYes:432418492889694210> **| Üye artık Ban'lı değil!**").then(msg => msg.delete({timeout: 20000}));
   message.guild.unban(bUser)
 }
 
