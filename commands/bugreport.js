6fec1844c836dc65c8e4203d74543df9c85b8ae8
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-06-06 23:13:01 +0300

    Update bugreport.js

--- commands/bugreport.js
+++ commands/bugreport.js
@@ -5,14 +5,14 @@ let user = message.author.username;
 let guild = message.guild.name;
 let channel = bot.channels.get("445069238676357120")
 let embed = new Discord.RichEmbed()
-.setTitle("Bug Report")
+.setTitle("Hata Buldum")
 .setThumbnail("https://images-ext-1.discordapp.net/external/nQoe_5zRdR6A5gsh2fevRbNvhoc5A2YIWP7zVdN5_NE/%3Fv%3D1/https/cdn.discordapp.com/emojis/435908220100280320.png?width=80&height=80")
-.addField("Bug", bug)
-.addField("Reported By", user)
-.addField("Reported in", guild)
+.addField("Hata", bug)
+.addField("Bulan", user)
+.addField("Bulduğu Sunucu", guild)
 .setColor("#f49542")
 
-message.channel.send("<:tickYes:432418492889694210> **| Your bug has been reported in the official server. It will be reviewed so please be patient.**")
+message.channel.send(":white_check_mark: **| Bulduğun hata botun sunucusuna gönderildi. İnceliyoruz. En yakın zamanda düzelteceğiz. Bildirdiğiniz için teşekkürler.**")
 channel.send(embed).then(i => i.react("⏳"))
 
   
@@ -20,5 +20,5 @@ channel.send(embed).then(i => i.react("⏳"))
 
 }
 module.exports.help = {
-name: "bugreport"
+name: "bugbuldum"
 }
