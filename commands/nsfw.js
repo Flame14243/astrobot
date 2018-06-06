06a567e9d9008d376106a7e3cbf0935e8c64734c
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-06-07 00:13:59 +0300

    Update nsfw.js

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -3,13 +3,13 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
 let embed = new Discord.RichEmbed()
-.setTitle("NSFW")
-.setDescription("There are currently 5 commands in this category.")
-.addField("`" + message.prefix +"hentai`", "Get random hentai image.")
-.addField("`" + message.prefix +"ecchi`", "Get random ecchi image.")
-.addField("`" + message.prefix +"neko`", "Get random neko image.")
-.addField("`" + message.prefix +"boobs`", "Get random boobs image.")
-.addField("`" + message.prefix +"ass`", "Get random ass image.")
+.setTitle(":name_badge:  NSFW")
+.setDescription("Bu komutlar kullanmak için nfsw kanalınızın olması gerekmektedir. Komutları odada kimse yokken kullanın :)")
+.addField("`" + message.prefix +"hentai`", "Rastgele hentai resmi atar")
+.addField("`" + message.prefix +"ecchi`", "Rastgele ecchi resmi atar")
+.addField("`" + message.prefix +"neko`", "Rastgele neko resmi atar")
+.addField("`" + message.prefix +"meme`", "Rastgele meme resmi atar.")
+.addField("`" + message.prefix +"popo`", "Rastgele popo resmi atar")
 
 
 message.channel.send(embed);
