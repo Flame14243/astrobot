4b59b970beeed94c3f3b407c1269b17c6d2cb2f8
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-06-06 23:20:10 +0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -3,30 +3,30 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   
   let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!kUser) return message.channel.send("Can't find user!");
-  if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("<:tickNo:432418492667396097> **| You don't have `KICK_MEMBERS` permissions.**")
+  if(!kUser) return message.channel.send(":name_badge: **| Üye bulunamadı.**");
+  if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send(":no_entry_sign: **| Senin `KICK_MEMBERS` yetkin yok.**")
   let kReason = args.join(" ").slice(22);
-  if(!kReason) return message.channel.send("Please provide a reason!")
+  if(!kReason) return message.channel.send(":pencil2: **| Lütfen bir sebep belirtin!**")
 
   let kickEmbed = new Discord.RichEmbed()
-  .setDescription("Kick")
+  .setDescription("**KICK**")
   .setColor("#f45642")
-  .addField("User", `${kUser}`)
-  .addField("Moderator", `${message.author}`)
-  .addField("Reason", kReason)
+  .addField("Üye", `${kUser}`)
+  .addField("Yetkili", `${message.author}`)
+  .addField("Sebep", kReason)
   .setTimestamp();
 
-  let kickChannel = message.guild.channels.find(`name`, 'mod-log');
-  if(!kickChannel) return message.channel.send("<:tickNo:432418492667396097> **| Can't find `mod-log` channel.**");
+  let kickChannel = message.guild.channels.find(`name`, 'eoa_kick');
+  if(!kickChannel) return message.channel.send(":name_badge: **| Sunucuda `eoa_kick` kanalı bulunamadı.**");
    let embed = new Discord.RichEmbed()
-  .setDescription("Kick")
+  .setDescription("**KICK**")
   .setColor("#f45642")
-  .addField("User", kUser.user.tag)
-  .addField("Moderator", message.author.tag)
-  .addField("Time", message.createdAt.toLocaleString())
-  .addField("Reason", kReason);
+  .addField("Üye", kUser.user.tag)
+  .addField("Yetkili", message.author.tag)
+  .addField("Saat", message.createdAt.toLocaleString())
+  .addField("Sebep", kReason);
   kUser.send(embed)
-message.channel.send("<:tickYes:432418492889694210> **| That member is kicked.**")
+message.channel.send(":white_check_mark:  **| Üye başarıyla sunucudan atıldı.**")
 kUser.kick(kReason)
 kUser.send(embed);
 kickChannel.send(kickEmbed);
